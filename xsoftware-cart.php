7a180c4b93e1549cbe0cc97e071f367f2dd075d2
Luca Gasperini <info@xsoftware.it>
2019-08-15 18:30:16 +0200

    Hotfix payment button

--- xsoftware-cart.php
+++ xsoftware-cart.php
@@ -129,7 +129,9 @@ class xs_cart_plugin
                         /* Print the sale order by xs_cart_sale_order_html filter */
                         $html .= apply_filters('xs_cart_sale_order_html', $so);
                         /* Print the payment link */
-                        $html .= apply_filters( 'xs_cart_approval_link', $so );
+                        $payment = apply_filters( 'xs_cart_approval_link', $so );
+                        $html .= $payment['html'];
+                        unset($payment['html']);
                 /* If cart is empty or is not set print the html page for empty cart */
                 } else {
                         return apply_filters('xs_cart_empty_html', NULL);
